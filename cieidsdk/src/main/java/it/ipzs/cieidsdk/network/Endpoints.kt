13e48be61117ba125acab1c4f61afef8f712a2aa
Diego Turchi <diego.turchi@libero.it>
2020-03-11 17:39:23 +0100

    Initial project import

---
+++ cieidsdk/src/main/java/it/ipzs/cieidsdk/network/Endpoints.kt
@@ -0,0 +1,10 @@
+package it.ipzs.cieidsdk.network
+
+internal object Endpoints {
+
+    const val idp = "Authn/SSL/Login2"
+
+    const val BASE_URL_CERTIFICATE = "idserver.servizicie.interno.gov.it"
+    const val PIN_ROOT = "sha256/Vjs8r4z+80wjNcr1YKepWQboSIRi63WsWXhIMN+eWys="
+
+}
